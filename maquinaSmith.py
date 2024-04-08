65e58ac4673843bf9fa5ea55029cf2106f34c75b
TonaSanchez <tonagsn@gmail.com>
2024-04-07 18:59:39 -0600

    Se crea el archivo

---
+++ maquinaSmith.py
@@ -0,0 +1,24 @@
+class MaquinaSmith():
+    def __init__():
+        pass
+        
+
+    def sentadilla():
+        conteo = 0
+        desicion = input("¿Quiere usted hacer una sentadilla? S ó N: ")
+
+        while desicion == "S" or desicion == "s":
+            conteo += 1 
+            print(conteo)
+            
+            desicion = input("¿Quiere usted hacer una sentadilla? S ó N: ")
+
+
+sentadilla = MaquinaSmith
+sentadilla.sentadilla()
+
+        
+    
+
+
+    
